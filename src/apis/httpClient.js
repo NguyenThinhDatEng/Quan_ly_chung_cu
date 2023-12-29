55f42355e6a5ea5b7ac976ce1f56489aa446978d
Tuấn Anh <92778429+ntacsharp@users.noreply.github.com>
2023-12-29 09:46:54 +0700

    x10 timeout baby

--- src/apis/httpClient.js
+++ src/apis/httpClient.js
@@ -5,7 +5,7 @@ const ApplicationJson = "application/json";
 // tạo 1 axios instances
 const httpClient = axios.create({
   baseURL: "https://localhost:4001/api/",
-  timeout: 30000, // Nếu kết nối có vấn đề -> tránh treo kết nối mãi mãi
+  timeout: 300000, // Nếu kết nối có vấn đề -> tránh treo kết nối mãi mãi
   headers: {
     "Content-Type": ApplicationJson,
     "Authorization": "Bearer " + sessionStorage.getItem("userToken"),
